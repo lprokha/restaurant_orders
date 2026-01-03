1ee74dcedcbb0c0c85795cdec31ceaf99cba7285
Leyla P. <liya1998@list.ru>
2026-01-04 03:52:05 +0600

    Added pre-made files to the project, updated .gitignore

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // это для домашки
+        // выберите любое количество заказов, какое вам нравится.
+
+        //var orders = RestaurantOrders.read("orders_100.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
+        //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
+
+        // протестировать ваши методы вы можете как раз в этом файле (или в любом другом, в котором вам будет удобно)
+    }
+}
